9c7365e566552bc0f95b798a79fff084ae046da5
ajitvibhute123 <vibhuteajit3@gmail.com>
2022-08-01 22:39:26 +0530

    Added:U-2 Validations is done on Payroll DTO

--- src/main/java/com/bridgelabz/addressbookapp/model/AddressBookData.java
+++ src/main/java/com/bridgelabz/addressbookapp/model/AddressBookData.java
@@ -3,10 +3,18 @@ package com.bridgelabz.addressbookapp.model;
 import com.bridgelabz.addressbookapp.dto.AddressBookDTO;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name ="contact_details")
 public @Data class AddressBookData {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "person_Id")
     private int personId;
     private String name;
     private String phoneNumber;
@@ -18,6 +26,10 @@ public @Data class AddressBookData {
 
     public AddressBookData(int personId, AddressBookDTO addressbookDTO) {
         this.personId = personId;
+        this.updateAddressBookdata(addressbookDTO);
+
+    }
+    public void updateAddressBookdata(AddressBookDTO addressbookDTO) {
         this.name = addressbookDTO.name;
         this.phoneNumber = addressbookDTO.phoneNumber;
         this.address = addressbookDTO.address;
