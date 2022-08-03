52da73fd4fbfe02828b9ac598f362283e7ab98d2
ajitvibhute123 <vibhuteajit3@gmail.com>
2022-08-03 23:21:06 +0530

    Added : with Model Mapper without using constructor in Employee payroll service data class

--- src/main/java/com/bridgelabz/addressbookapp/AddressBookApplApplication.java
+++ src/main/java/com/bridgelabz/addressbookapp/AddressBookApplApplication.java
@@ -1,6 +1,8 @@
 package com.bridgelabz.addressbookapp;
 
 import lombok.extern.slf4j.Slf4j;
+
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -15,18 +17,24 @@ public class AddressBookApplApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book Application Program");
-//		SpringApplication.run(AddressBookAppApplication.class, args);
+		SpringApplication.run(AddressBookAppApplication.class, args);
 		ApplicationContext context = SpringApplication.run(AddressBookApplApplication.class, args);
 		log.info("AddressBook App Started in {} Environment",context.getEnvironment().getProperty("environment"));
 		log.info("AddressBook App started");
 
 	}
-	//@Bean
-//	public Docket productApi() {
-//		return new Docket(DocumentationType.SWAGGER_2)
-//				.select()
-//				.apis(RequestHandlerSelectors.basePackage("com.bridgelabz.addressbookapp"))
-//				.build();
-//	}
+	@Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.any())              
+          .paths(PathSelectors.any())                          
+          .build();                                           
+    }
+	
+	@Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
 
 }
